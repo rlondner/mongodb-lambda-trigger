15dcfd129f900f7672bee9675020154178cbf296
rlondner <raphael+github@mongodb.com>
2018-01-05 13:45:20 -0800

    anonymous functions

--- trigger/listen.js
+++ trigger/listen.js
@@ -1,7 +1,7 @@
 const MongoClient = require("mongodb");
-var storage = require("node-persist");
-const EJSON = require('mongodb-extjson')
-var url = require("./config.js").mongoDBUrl;
+const storage = require("node-persist");
+const EJSON = require("mongodb-extjson");
+const url = require("./config.js").mongoDBUrl;
 const matchStage = {
   $match: { "fullDocument.device.celsiusTemperature": { $gt: 15 } }
 };
@@ -17,22 +17,27 @@ MongoClient.connect(url, (err, client) => {
   let changeStream = coll.watch([matchStage]);
   // const changeStream = coll.watch();
 
-  storage.init({ dir: "localStorage" }).then(function() {
+  storage.init({ dir: "localStorage" }).then(() => {
     if (true) {
-      storage.getItem(CS_TOKEN).then(
-        function(token) {
-          if (token !== undefined) {
-            console.log(`using resume token: ${token}`);
-            changeStream = coll.watch([matchStage], { resumeAfter: EJSON.parse(token)});
+      storage
+        .getItem(CS_TOKEN)
+        .then(
+          token => {
+            if (token !== undefined) {
+              console.log(`using resume token: ${token}`);
+              changeStream = coll.watch([matchStage], {
+                resumeAfter: EJSON.parse(token)
+              });
+            }
+          },
+          err => {
+            console.log("error retrieving change stream resume token: " + err);
           }
-        },
-        function(err) {
-          console.log("error retrieving change stream resume token: " + err);
-        }
-      ).then(function() {
-        pollStream(changeStream, storage);
-      });
-      
+        )
+        .then(() => {
+          console.log("polling change stream...");
+          pollStream(changeStream, storage);
+        });
     } else {
       changeStream.on("change", c => console.log(c));
     }
@@ -40,13 +45,11 @@ MongoClient.connect(url, (err, client) => {
 });
 
 function pollStream(cs, storage) {
-  console.log('polling change stream...')
+  console.log("waiting for change stream...");
   cs.next((err, change) => {
     if (err) return console.log(err);
     resumeToken = EJSON.stringify(change._id);
-    storage.setItem(CS_TOKEN, resumeToken).then(function() {
-      console.log(change);
-    });
+    storage.setItem(CS_TOKEN, resumeToken).then(console.log(change));
     pollStream(cs, storage);
   });
 }
